f2bfa2ebe60ac3e8c4cca0996532a8da93ccca1f
Max Starr <max@maxstarr.xyz>
2024-06-29 19:54:07 -0500

    first commit

---
+++ scripts/add-character.js
@@ -0,0 +1,200 @@
+import { XPTrackerSettings } from "./model/settings.js"
+import { AddCharacterFormApplication } from "./controllers/add-character.js"
+import { ChangeSummaryDialog } from "./controllers/change-summary.js"
+import { EditCharacterFormApplication } from "./controllers/edit-character.js"
+import { RewardXPFormApplication } from "./controllers/reward-xp.js"
+import { SettingsForm } from "./controllers/settings-form.js"
+import { XPTrackerApplication } from "./controllers/xp-tracker.js"
+import { XPTrackerData } from "./model/data.js"
+
+export class XPTracker {
+  getSceneControlButtons(sceneControlButtons) {
+    const notesButton = sceneControlButtons.find(
+      (button) => button.name === "notes",
+    )
+
+    notesButton.tools.push({
+      name: "xp-tracker",
+      title: "XP Tracker",
+      icon: "fas fa-chart-line",
+      visible: true,
+      onClick: () => {
+        this.application.render(true, {
+          focus: true,
+        })
+      },
+    })
+  }
+
+  /**
+   * @typedef {Object} Character
+   * @property {string} name
+   * @property {number} xp
+   */
+
+  constructor() {
+    this.data = new XPTrackerData()
+    this.rounding = Math.floor
+    this.sortBy = "default"
+    this.sortOrder = "asc"
+  }
+
+  async initialize(initialData = []) {
+    await this.data.initialize(initialData)
+
+    this.settings = new XPTrackerSettings(this)
+
+    this.application = new XPTrackerApplication({
+      data: this.data,
+      trackerInstance: this,
+    })
+
+    Hooks.on(
+      "getSceneControlButtons",
+      this.getSceneControlButtons.bind(this),
+    )
+    
+  }
+
+  async addCharacter(character) {
+    await this.data.addCharacter({ ...character, id: crypto.randomUUID() })
+  }
+
+  async editCharacter(characterId, newCharacter) {
+    const changes = []
+    const journalData = this.data.getJournalEntryData()
+    const character = journalData.find(
+      (character) => character.id === characterId,
+    )
+    if (character.name !== newCharacter.name) {
+      changes.push({
+        property: "Name",
+        old: character.name,
+        new: newCharacter.name,
+      })
+    }
+
+    if (character.xp !== newCharacter.xp) {
+      changes.push({
+        property: "XP",
+        old: character.xp,
+        new: newCharacter.xp,
+      })
+    }
+
+    new ChangeSummaryDialog([
+      {
+        name: character.name,
+        changes,
+      },
+    ]).render(true, {
+      focus: true,
+    })
+
+    await this.data.editCharacter(characterId, newCharacter)
+  }
+
+  showNewCharacterForm() {
+    new AddCharacterFormApplication(
+      {},
+      {
+        trackerInstance: this,
+      },
+    ).render(true, {
+      focus: true,
+    })
+  }
+
+  showRewardXPForm() {
+    new RewardXPFormApplication(
+      {},
+      {
+        trackerInstance: this,
+      },
+    ).render(true, {
+      focus: true,
+    })
+  }
+
+  showSettingsForm() {
+    new SettingsForm(
+      {},
+      {
+        trackerInstance: this,
+      },
+    ).render(true, {
+      focus: true,
+    })
+  }
+
+  setSortBy(sortBy) {
+    if (sortBy === this.sortBy) {
+      if (this.sortOrder === "desc") {
+        this.sortBy = "default"
+        this.application.render()
+        return
+      }
+
+      this.sortOrder = this.sortOrder === "asc" ? "desc" : "asc"
+    } else {
+      this.sortOrder = "asc"
+    }
+
+    this.sortBy = sortBy
+    this.application.render()
+  }
+
+  showEditCharacterForm(characterId) {
+    const character = this.data
+      .getJournalEntryData()
+      .find((character) => character.id === characterId)
+    new EditCharacterFormApplication(character, {
+      trackerInstance: this,
+    }).render(true, {
+      focus: true,
+    })
+  }
+
+  async rewardXp(xp, characters) {
+    const journalData = this.data.getJournalEntryData()
+    const changes = []
+    journalData.forEach((character) => {
+      if (!characters || characters.includes(character.id)) {
+        changes.push({
+          name: character.name,
+          changes: [
+            {
+              property: "XP",
+              old: character.xp,
+              new: character.xp + xp,
+            },
+          ],
+        })
+        character.xp += xp
+      }
+    })
+
+    new ChangeSummaryDialog(changes).render(true, {
+      focus: true,
+    })
+
+    await this.data._updateJournalEntry(journalData)
+  }
+
+  render() {
+    this.application.render(true)
+  }
+}
+
+Hooks.once("ready", async function () {
+  const xpTracker = new XPTracker()
+
+  await xpTracker.initialize()
+
+  xpTracker.settings.showOnStartup && xpTracker.render(true)
+})
+
+Hooks.once("devModeReady", ({ registerPackageDebugFlag }) => {
+  registerPackageDebugFlag(XPTracker.ID)
+})
+
---
+++ scripts/model/settings.js
@@ -0,0 +1,113 @@
+import { ID } from "./constants.js";
+import { XP_TIER_SCHEMES } from "./xp-tier-schema.js";
+
+export class XPTrackerSettings {
+  static ROUNDING_OPTIONS = {
+    FLOOR: {
+      name: "Down",
+      function: Math.floor,
+    },
+    ROUND: {
+      name: "Round",
+      function: Math.round,
+    },
+    CEIL: {
+      name: "Up",
+      function: Math.ceil,
+    },
+  };
+
+  static XP_TIER_SCHEMES = XP_TIER_SCHEMES;
+
+  static XP_TIER_SCHEME = {
+    id: "xpTierScheme",
+    name: "XP Tier Scheme",
+    default: Object.keys(XP_TIER_SCHEMES)[0],
+    type: String,
+    choices: Object.keys(XP_TIER_SCHEMES).reduce((acc, key) => {
+      acc[key] = XP_TIER_SCHEMES[key].name;
+      return acc;
+    }, {}),
+  };
+
+  static ROUNDING = {
+    id: "rounding",
+    name: "Rounding",
+    default: this.ROUNDING_OPTIONS.ROUND,
+    type: String,
+    choices: Object.keys(this.ROUNDING_OPTIONS).reduce((acc, key) => {
+      acc[key] = this.ROUNDING_OPTIONS[key].name;
+      return acc;
+    }, {}),
+  };
+
+  static SHOW_ON_STARTUP = {
+    id: "showOnStartup",
+    name: "Show on Startup",
+    default: true,
+    type: Boolean,
+  };
+
+  constructor(trackerInstance) {
+    this.trackerInstance = trackerInstance;
+    const settings = [
+      XPTrackerSettings.XP_TIER_SCHEME,
+      XPTrackerSettings.ROUNDING,
+      XPTrackerSettings.SHOW_ON_STARTUP,
+    ];
+
+    settings.forEach((setting) => {
+      game.settings.register(ID, setting.id, {
+        name: `XP Tracker ${setting.name}`,
+        hint: `XP Tracker ${setting.name}`,
+        scope: "world",
+        config: true,
+        default: setting.default,
+        type: setting.type,
+        choices: setting.choices,
+        onChange: (value) => {
+          this.trackerInstance[setting.id] = value;
+          this.trackerInstance.application.render();
+        },
+      });
+    });
+  }
+
+  get xpTierScheme() {
+    return XPTrackerSettings.XP_TIER_SCHEMES[
+      game.settings.get(ID, XPTrackerSettings.XP_TIER_SCHEME.id)
+    ];
+  }
+
+  set xpTierScheme(value) {
+    if (!XPTrackerSettings.XP_TIER_SCHEMES[value]) {
+      throw new Error(
+        `Tried to set xpTierScheme to ${value}, but it is not a valid option`,
+      );
+    }
+    game.settings.set(ID, XPTrackerSettings.XP_TIER_SCHEME.id, value);
+  }
+
+  get rounding() {
+    return XPTrackerSettings.ROUNDING_OPTIONS[
+      game.settings.get(ID, XPTrackerSettings.ROUNDING.id)
+    ].function;
+  }
+
+  set rounding(value) {
+    if (!XPTrackerSettings.ROUNDING_OPTIONS[value]) {
+      throw new Error(
+        `Tried to set rounding to ${value}, but it is not a valid option`,
+      );
+    }
+    game.settings.set(ID, XPTrackerSettings.ROUNDING.id, value);
+  }
+
+  get showOnStartup() {
+    return game.settings.get(ID, XPTrackerSettings.SHOW_ON_STARTUP.id);
+  }
+
+  set showOnStartup(value) {
+    game.settings.set(ID, XPTrackerSettings.SHOW_ON_STARTUP.id, value);
+  }
+}
